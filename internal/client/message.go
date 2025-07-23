09ade02182def3701daac1b5f1b3714cd3252640
Heidarie <68645579+Heidarie@users.noreply.github.com>
2025-07-23 08:54:48 +0100

    Create message.go

---
+++ internal/client/message.go
@@ -0,0 +1,9 @@
+package client
+
+import "encoding/json"
+
+type Message struct {
+	Type    string          `json:"type"`
+	Sender  string          `json:"sender,omitempty"`
+	Payload json.RawMessage `json:"payload,omitempty"`
+}
